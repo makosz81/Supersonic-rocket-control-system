b6dc041e9fe2c26dc7b800815a1be685407569c1
makosz81 <maciej.makowski@live.com>
2016-01-24 19:08:44 -0600

    Update Control.c

--- Control.c
+++ Control.c
@@ -1,53 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
-#include "Lib/sensor.c" //Location of main_functions.c
+#include "/home/pi/lib/main_functions" //Location of main_functions.c
 
 #define DT 0.02
 #define G_GAIN 0.070
 
 int main(int argc, char *argv[])
 {
+	//set up both servos and gyro using main_functions
 	gyroSetup();
 	servoSetup();
 
-	int i = 50;
+	// rate of rotation about the axis
+	float rate_gyr_y = 0.0;   
+	float rate_gyr_x = 0.0;    
+	float rate_gyr_z = 0.0;     
 
-	float rate_gyr_y = 0.0;   // [deg/s]
-	float rate_gyr_x = 0.0;    // [deg/s]
-	float rate_gyr_z = 0.0;     // [deg/s]
-
-	int  accRaw[3];
-	int  magRaw[3];
+	//Raw Gyroscope data
 	int  gyrRaw[3];
-
+	
+	//Orientation
 	float gyroXangle = 0.0;
 	float gyroYangle = 0.0;
 	float gyroZangle = 0.0;
 
-	struct  timeval tvBegin, tvEnd,tvDiff;
-
 	while(1)
 	{
-	readGYR(gyrRaw);
+		readGYR(gyrRaw);
 
-	//Convert Gyro raw to degrees per second
-	rate_gyr_x = (float) gyrRaw[0]  * G_GAIN;
-	rate_gyr_y = (float) gyrRaw[1]  * G_GAIN;
-	rate_gyr_z = (float) gyrRaw[2]  * G_GAIN;
+		//Convert Gyro raw to degrees per second
+		rate_gyr_x = (float) gyrRaw[0]  * G_GAIN;
+		rate_gyr_y = (float) gyrRaw[1]  * G_GAIN;
+		rate_gyr_z = (float) gyrRaw[2]  * G_GAIN;
 
-	//Calculate the angles from the gyro
-	gyroXangle+=rate_gyr_x*DT;
-	gyroYangle+=rate_gyr_y*DT;
-	gyroZangle+=rate_gyr_z*DT;
+		//Calculate the orientation from the gyroscope
+		gyroXangle+=rate_gyr_x*DT;
+		gyroYangle+=rate_gyr_y*DT;
+		gyroZangle+=rate_gyr_z*DT;
 	
-//Used for debugging
-//	printf("%f, %f, %f\n",gyroXangle,gyroYangle,gyroZangle);
-//	printf("%f, %f, %f\n",rate_gyr_x,rate_gyr_y,rate_gyr_z);
-//	printf("%d, %d, %d\n",gyrRaw[0],gyrRaw[1],gyrRaw[2]);
+		//Used for debugging 
+		//printf("%f, %f, %f\n",gyroXangle,gyroYangle,gyroZangle);
+		//printf("%f, %f, %f\n",rate_gyr_x,rate_gyr_y,rate_gyr_z);
+		//printf("%d, %d, %d\n",gyrRaw[0],gyrRaw[1],gyrRaw[2]);
 
 
-//Moves the servo
-	system("echo 0=50% /dev/servoblaster");
+		//Moves the servo
+		system("echo 0=50% /dev/servoblaster");
      }
 }
